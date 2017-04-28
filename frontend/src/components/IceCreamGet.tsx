727d4b8d2712e19b2b01d7df80438459d25d5bae
David Joseph Vitale <davidjosephvitale@gmail.com>
2017-04-27 21:53:48 -0500

    Added Ice Cream Type in the new React Component, prints out generic object on screen

--- frontend/src/components/IceCreamGet.tsx
+++ frontend/src/components/IceCreamGet.tsx
@@ -1,9 +1,18 @@
 import * as React from "react"
 
+import { IceCream, IceCreamInterface } from "../../node_modules/shared"
+
 import "../styles/Hello.less"
 
-export class IceCreamGet extends React.Component<undefined, undefined> {
+export interface IceCreamGetProps { IceCreams : IceCreamInterface[] }
+
+export class IceCreamGet extends React.Component<undefined, IceCreamGetProps> {
+    constructor(props : any){
+        super(props)
+        this.state = {IceCreams : [ new IceCream("Chocolate Chip", false, true)]}
+    }
+
     render() {
-        return <h1> Hello Ice Cream! </h1>
+        return <h1>Hello Ice Cream! {JSON.stringify(this.state.IceCreams)} </h1>
     }
 }
