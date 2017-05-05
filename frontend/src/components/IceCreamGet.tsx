56ee5bd79b31c7236578cb9d080adf5b62d06ed1
David Joseph Vitale <davidjosephvitale@gmail.com>
2017-05-05 18:08:27 -0500

    Got frontend synced up with backend!

--- frontend/src/components/IceCreamGet.tsx
+++ frontend/src/components/IceCreamGet.tsx
@@ -4,33 +4,28 @@ import { IceCream, IceCreamInterface } from "../../node_modules/shared"
 
 import "../styles/Hello.less"
 
-import 'whatwg-fetch'
+//import 'whatwg-fetch'
 
 export interface IceCreamGetProps { IceCreams : IceCreamInterface[] }
 
-export class IceCreamGet extends React.Component<undefined, IceCreamGetProps> {
+export class IceCreamGet extends React.Component<undefined, IceCreamGetProps>{
     constructor(props : any){
         super(props)
-        this.state = {IceCreams : [new IceCream("Chocolate Chip", false, true)]}
-        this.handleClick = this.handleClick.bind(this)
-    }
+        this.state = {IceCreams : []}
+        this.handleClick = this.handleClick.bind(this)}
 
     handleClick() {
         fetch("http://localhost:3000/api/IceCream").then((response) => {
             response.json().then((data : any) => {
-                this.setState(function(prevState : IceCreamGetProps) {
-                    prevState.IceCreams.push(this.jsonToIceCream(data))
-                    return prevState})})})}
+                this.setState({IceCreams : this.jsonToIceCreams(data)})})})}
 
-    jsonToIceCream(json : any) : IceCreamInterface {
-        //return <IceCreamInterface extends {Name: string}> {Name: "ok"}
-      return new IceCream("test", true, true)
-    }
+    jsonToIceCreams(jsonData : any) : IceCreamInterface[] {
+        return jsonData as IceCreamInterface[]}
 
     render() {
         return (
             <div className="IceCreamDisplay">
             <button onClick={this.handleClick}>{"Get From Server"}</button>
-            Delicious Ice Cream {JSON.stringify(this.state.IceCreams)}
+            Delicious Ice Creams: {JSON.stringify(this.state.IceCreams)}
             </div>
-        );}}
+        )}}
